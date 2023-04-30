d23ca9dfb0e115987a1b05a2f763037c27557430
Janybek <best.joni.547545@gmail.com>
2023-04-30 17:11:57 +0600

    add current user props

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { User } from '@prisma/client'
 import { FC } from 'react'
 
 import Container from '../Container'
@@ -8,7 +9,11 @@ import Logo from './Logo'
 import Search from './Search'
 import UserMenu from './UserMenu'
 
-const Navbar: FC = () => {
+interface NavbarProps {
+  currentUser?: User | null
+}
+
+const Navbar: FC<NavbarProps> = ({ currentUser }) => {
   return (
     <header className='fixed z-10 w-full bg-white shadow-sm'>
       <div className='border-b-[1px] py-4'>
@@ -16,7 +21,7 @@ const Navbar: FC = () => {
           <nav className='flex flex-row items-center justify-between gap-3 md:gap-0'>
             <Logo />
             <Search />
-            <UserMenu />
+            <UserMenu currentUser={currentUser} />
           </nav>
         </Container>
       </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,37 @@
 import { Nunito } from 'next/font/google'
 
 import ClientOnly from '@/app/components/ClientOnly'
+import LoginModal from '@/app/components/Modals/LoginModal'
 import RegisterModal from '@/app/components/Modals/RegisterModal'
 import Navbar from '@/app/components/navbar/Navbar'
 import ToasterProvider from '@/app/providers/ToasterProvider'
 
+import getCurrentUser from './actions/getCurrentUser'
 import './globals.css'
 
 export const metadata = {
   title: 'Airbnb',
-  description: 'Airbnb clone'
+  description: 'Airbnb clone',
 }
 
 const font = Nunito({
-  subsets: ['latin']
+  subsets: ['latin'],
 })
 
-export default function RootLayout({
-  children
+export default async function RootLayout({
+  children,
 }: {
   children: React.ReactNode
 }) {
+  const currentUser = await getCurrentUser()
   return (
     <html lang='en'>
       <body className={font.className}>
         <ClientOnly>
           <ToasterProvider />
+          <LoginModal />
           <RegisterModal />
-          <Navbar />
+          <Navbar currentUser={currentUser} />
         </ClientOnly>
         {children}
       </body>
